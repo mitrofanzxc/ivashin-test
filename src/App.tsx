02db1b8f2da71cde1f827dc2b074f489159c9a2f
mitrofanzxc <mitrofanzxc@gmail.com>
2022-11-20 16:03:38 +0300

    init: start of the test task

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <>
+      <header>Header</header>
+      <main>Main</main>
+    </>
+  );
+};
+
+export { App };
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react';
+import { HashRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import './index.scss';
+
+const container = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
